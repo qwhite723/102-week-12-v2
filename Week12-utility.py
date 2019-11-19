3fb09959c25d7af652b3ccdda997143e405b140f
Quintin White <qwhite@mymail.mines.edu>
2019-11-19 11:56:46 -0700

    third

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,2 +1,14 @@
 def PrintOutput(word):
     print("OUTPUT",word)
+    
+def LoadFile(filename):
+    with open(filename, 'r') as file:
+        read = file.readlines()
+    return read
+
+def UpdateString(s1, s2, i):
+    split = []
+    for char in s1:
+        split.append(char)
+    split[i] = s2
+    print("OUTPUT", split)
